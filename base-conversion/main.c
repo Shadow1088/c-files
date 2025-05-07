e42a93ab6abfdb24200d20e47e329ed5ec670b1c
ZZ <shadow1088punch@gmail.com>
2025-05-07 08:32:25 +0200

    minor changes

--- base-conversion/main.c
+++ base-conversion/main.c
@@ -31,14 +31,30 @@ void autoDetect(BASENUM *num){
             if ((*num).value[i]<'2' && (*num).type=='u'){
                 (*num).type = 'b';
             }
+
             if ((*num).value[i]>='a' && (*num).value[i]<='f'){
                 (*num).type = 'x';
+                for(int j=0; j<(*num).length;j++){
+                    if ((*num).value[j]>'f'){
+                        (*num).type = 'u';
+                        break;
+                    }
+                }
+                break;
             }
-            if ((*num).value[i]>'7' && (*num).type!='x'){
+            if ((*num).value[i]>'7' && (*num).value[i]<'9' && (*num).type!='x'){
                 (*num).type = 'd';
+                continue;
             }
-            if ((*num).value[i]>'1' && (*num).type!='d' && (*num).type!='x'){
+            if ((*num).value[i]>'1' && (*num).value[i]<'8' && (*num).type!='d' && (*num).type!='x' && (*num).length%3==0){
+                printf("%d", (*num).length);
                 (*num).type = 'o';
+                continue;
+            }
+
+            if ((*num).type=='b' && (*num).value[i] >'2'){
+                (*num).type = 'd';
+                break;
             }
         }
     }
@@ -51,9 +67,171 @@ void autoDetect(BASENUM *num){
 }
 
 
+BASENUM convertBD(BASENUM num){
+    BASENUM result;
+    result.type = 'd';
+
+    return result;
+}
+
+BASENUM convertBX(BASENUM num){
+    BASENUM result;
+    result.type = 'x';
+
+    return result;
+}
+
+BASENUM convertBO(BASENUM num){
+    BASENUM result;
+    result.type = 'o';
+
+    return result;
+}
+
+BASENUM convertDB(BASENUM num){
+    BASENUM result;
+    result.type = 'b';
+
+    return result;
+}
+
+BASENUM convertDX(BASENUM num){
+    BASENUM result;
+    result.type = 'x';
+
+    return result;
+}
+
+BASENUM convertDO(BASENUM num){
+    BASENUM result;
+    result.type = 'o';
+
+    return result;
+}
+
+BASENUM convertXD(BASENUM num){
+    BASENUM result;
+    result.type = 'd';
+
+    return result;
+}
+
+BASENUM convertXB(BASENUM num){
+    BASENUM result;
+    result.type = 'b';
+
+    return result;
+}
+
+BASENUM convertXO(BASENUM num){
+    BASENUM result;
+    result.type = 'o';
+
+    return result;
+}
+
+BASENUM convertOD(BASENUM num){
+    BASENUM result;
+    result.type = 'd';
+
+    return result;
+}
+
+BASENUM convertOB(BASENUM num){
+    BASENUM result;
+    result.type = 'b';
+
+    return result;
+}
+
+BASENUM convertOX(BASENUM num){
+    BASENUM result;
+    result.type = 'x';
+
+    return result;
+}
+
+
+BASENUM convert(BASENUM num, char type){
+    switch (type) {
+        case 'b':
+            switch (num.type) {
+                case 'b':
+                    printf("%s is already binary\n", num.value);
+                    break;
+                case 'd':
+                    return convertDB(num);
+                case 'x':
+                    return convertXB(num);
+                case 'o':
+                    return convertOB(num);
+            }
+            break;
+
+        case 'd':
+            switch (num.type) {
+                case 'b':
+                    return convertBD(num);
+                case 'd':
+                    printf("%s is already decimal\n", num.value);
+                    break;
+                case 'x':
+                    return convertXD(num);
+                case 'o':
+                    return convertOD(num);
+            }
+            break;
+
+        case 'x':
+            switch (num.type) {
+                case 'b':
+                    return convertBX(num);
+                case 'd':
+                    return convertDX(num);
+                case 'x':
+                    printf("%s is already hexadecimal\n", num.value);
+                    break;
+                case 'o':
+                    return convertOX(num);
+            }
+            break;
+
+        case 'o': // Target: octal
+            switch (num.type) {
+                case 'b':
+                    return convertBO(num);
+                case 'd':
+                    return convertDO(num);
+                case 'x':
+                    return convertXO(num);
+                case 'o':
+                    printf("%s is already octal\n", num.value);
+                    break;
+            }
+            break;
+    }
+
+    return num;
+}
+
+// asks user what base do we convert our string to
+char convertto(){
+    printf("What to convert to?");
+    char c = getchar();
+    while (!(c=='\n' || (c == 'x' || c == 'o' || c == 'd' || c == 'b'))){
+        printf("Invalid input, (x; b; d; o;)\nWhat to convert to?\n");
+
+        while (getchar() != '\n');
+        c = getchar();
+
+    }
+    return c;
+}
+
 int main(){
     BASENUM num = getValue();
     autoDetect(&num);
-    printf("%s\n", num.value);
+    BASENUM result = convert(num, convertto());
+    printf("%c\n", num.type);
     return 0;
 }
